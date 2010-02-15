b3e30807e4c7bb74b9eb5df9de85046c9460d378
mat_aki <matsumura.aki@gmail.com>
2010-02-15 18:47:28 +0900

    Investigate mixi params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
+require 'oauth/request_proxy/action_controller_request'
+require 'oauth/signature/hmac/sha1'
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
@@ -10,11 +12,21 @@ class ApplicationController < ActionController::Base
 
   mobile_filter :hankaku => true
 
+  before_filter :valid_mixi_app_mobile_request?
+
   helper_method :current_user
 
 private
   # FIXME: mock
   def current_user
     User.last
+  end
+
+  def valid_mixi_app_mobile_request?
+    logger.info request.headers.map{|k,v| "{#{k}:#{v}}"}.join(" : ")
+    oauth_request = OAuth::RequestProxy.proxy(request)
+    logger.info OAuth::Signature.sign(oauth_request, :consumer_secret => ENV['CONSUMER_SECRET'])
+  rescue => e
+    logger.info e
   end
 end
