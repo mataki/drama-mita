8f290f723e0c5fbdfd5f7cc7b1dd7dbe0b26a78b
mat_aki <matsumura.aki@gmail.com>
2010-02-15 16:58:29 +0900

    Use mobile filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
 
+  mobile_filter :hankaku => true
+
   helper_method :current_user
 
 private
