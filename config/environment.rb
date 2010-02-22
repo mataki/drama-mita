a431c8471657dc390f108dceb29089aa31b27197
mat_aki <matsumura.aki@gmail.com>
2010-02-22 15:48:25 +0900

    Use will_paginate from gemcutter

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "searchlogic"
   config.gem "oauth"
   config.gem "hashie"
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version=> '>=2.3.6', :source => 'http://gems.github.com/'
+  config.gem 'will_paginate'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
