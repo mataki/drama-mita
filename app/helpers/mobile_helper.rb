58d04f4daab2a40364a3be466b985cb41c4743f8
mat_aki <matsumura.aki@gmail.com>
2010-02-22 21:27:42 +0900

    Change design

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -2,17 +2,17 @@ module MobileHelper
   def font_size(size = :middle)
     case size
     when :large
-      base = 'font-size: 16px;'
+      base = 'font-size: 14px;'
       case request.mobile
-      when Jpmobile::Mobile::Docomo then 'font-size: xx-large;'
-      when Jpmobile::Mobile::Softbank then 'font-size: xx-large;'
+      when Jpmobile::Mobile::Docomo then 'font-size: large;'
+      when Jpmobile::Mobile::Softbank then 'font-size: large;'
       else
         base
       end
     when :small
       base = 'font-size: 10px;'
       case request.mobile
-      when Jpmobile::Mobile::Docomo then 'font-size: xx-small;'
+      when Jpmobile::Mobile::Docomo then 'font-size: small;'
       when Jpmobile::Mobile::Softbank then 'font-size: small;'
       else
         base
