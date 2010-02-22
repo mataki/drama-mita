c1b279f009748e7ea457095fd84585a2fea488ca
mat_aki <matsumura.aki@gmail.com>
2010-02-22 15:57:36 +0900

    Fix for postgress

--- app/models/drama.rb
+++ app/models/drama.rb
@@ -2,14 +2,14 @@ class Drama < ActiveRecord::Base
   attr_accessible :title
 
   belongs_to :category
-  has_many :episodes
+  has_many :episodes, :order => "num ASC"
   has_many :watches, :through => :episodes
 
   cattr_reader :per_page
   @@per_page = 5
 
   named_scope :populer, lambda {
-    {:select => "sum(watches_count) AS sum_watches_count, dramas.*", :joins => :episodes, :group => "drama_id", :order => "sum_watches_count DESC"}
+    {:select => "sum(CASE WHEN episodes.watches_count is null THEN 0 ELSE episodes.watches_count END) AS sum_watches_count, dramas.*", :joins => :episodes, :group => "dramas.id, dramas.title, dramas.category_id, dramas.created_at, dramas.updated_at, dramas.episodes_count", :order => "sum_watches_count DESC"}
   }
 
   def self.current_episodes
