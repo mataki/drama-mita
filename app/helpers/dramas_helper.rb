395b4f2968b54e429b14196b1cce37c9e3b7b3d7
mat_aki <matsumura.aki@gmail.com>
2010-02-25 11:55:36 +0900

    Show drama title with image

--- app/helpers/dramas_helper.rb
+++ app/helpers/dramas_helper.rb
@@ -1,5 +1,14 @@
 module DramasHelper
 
+  def drama_title_image(drama, show_drama_title_image = true)
+    @content_for_drama_title_image = link_to title_image_tag(drama), drama
+    @show_drama_title_image = show_drama_title_image
+  end
+
+  def show_drama_title_image?
+    @show_drama_title_image
+  end
+
   def title_image_tag(drama, options = {})
     return "" if drama.blank? or drama.title_image.blank?
     options = { :style => "border:none;"}
