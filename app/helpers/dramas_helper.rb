2732315550ad23d7f92cf9f5f999bd99b6c9e72e
mat_aki <matsumura.aki@gmail.com>
2010-02-25 18:17:46 +0900

    Fix design error on au, docomo

--- app/helpers/dramas_helper.rb
+++ app/helpers/dramas_helper.rb
@@ -11,7 +11,7 @@ module DramasHelper
 
   def title_image_tag(drama, options = {})
     return "" if drama.blank? or drama.title_image.blank?
-    options = { :style => "border:none;"}
+    options = { :style => "border:none;"}.update(options)
     image_tag("title_images/#{drama.title_image}", options)
   end
 end
