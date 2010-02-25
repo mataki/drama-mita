856d7698ed69ae3e6c83983b3f563324595179c6
mat_aki <matsumura.aki@gmail.com>
2010-02-25 15:54:39 +0900

    New installed user view welcome page

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,4 +1,6 @@
 class InfoController < ApplicationController
+  skip_before_filter :require_user, :only => :welcome
+
   def welcome
     @current_user = current_user || User.create_by_mixi_id!(params[:opensocial_owner_id])
   end
